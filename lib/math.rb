95eeebc3c64b43ade2cc96cd0d237f20985a7765
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-23 20:17:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,17 +9,21 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  result = num1 / num2
+  result
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  result
 end
 
 def modulo(num1, num2)
-
+  result = num1 % num2
+  result
 end
 
 def square_root(num)
-
+  result = sqrt(num)
+  result
 end
